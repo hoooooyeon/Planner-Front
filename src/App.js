06cacd6bc3f92272e5ddf30b573f4ccb265c4f15
bluebear <showmeii1201@naver.com>
2022-08-05 01:02:21 +0900

    서버와 연결 테스트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function App() {
+  const [result, setResult] = useState();
+
+  useEffect(() => {
+    axios.get("planner/").then((response) => {
+      setResult(response.data);
+    })
+      .catch((error) => {
+        setResult(error);
+      })
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {result}
     </div>
   );
 }
