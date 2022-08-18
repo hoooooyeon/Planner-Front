dc93a832eb32a3af9e9e4dd8977110e36e1f7ccc
hooooyeon <lhy3476@gmail.com>
2022-08-18 23:44:10 +0900

    슬라이더 버튼 완료

--- src/components/spot/SpotSlider.js
+++ src/components/spot/SpotSlider.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
-import SliderImg from './SliderImg';
 import { useRef, useState } from 'react';
 
 const SpotSliderBlock = styled.div`
@@ -11,10 +10,6 @@ const SpotSliderBlock = styled.div`
   border: 1px solid green;
   overflow: hidden;
   h1 {
-    z-index: 100;
-    position: absolute;
-    top: 80%;
-    left: 45%;
     color: white;
   }
 `;
@@ -22,6 +17,13 @@ const SpotSliderBlock = styled.div`
 const SliderImgs = styled.div`
   width: 3645px;
   height: 600px;
+`;
+
+const SliderImg = styled.div`
+  width: 1215px;
+  height: 600px;
+  background-color: lightgray;
+  float: left;
 `;
 
 const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
@@ -41,43 +43,46 @@ const NextButton = styled(StyledFontAwesomeIcon)`
   right: 11%;
 `;
 
+const TOTAL_SLIDES = 3;
 const SpotSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
   const imgsRef = useRef();
 
   const handleSlide = (currentIndex) => {
-    if (currentIndex > 3) {
+    if (currentIndex > TOTAL_SLIDES) {
       currentIndex = 1;
     } else if (currentIndex === 0) {
-      currentIndex = 3;
+      currentIndex = TOTAL_SLIDES;
     }
     setCurrentIndex(currentIndex);
+    imgsRef.current.style = 'transform: translateX(-' + 1215 * (currentIndex - 1) + 'px)';
+    imgsRef.current.style.transition = 'all 0.5s ease-in-out';
   };
 
   const handleSwipe = (direction) => {
     handleSlide(currentIndex + direction);
   };
-  const handlePrev = (direction) => {
-    handleSlide(currentIndex + direction);
-    console.log('prev' + currentIndex);
-    imgsRef.current.style =
-      'transform: translateX(-' + 1215 * currentIndex + 'px)';
-  };
-  const handleNext = (direction) => {
-    handleSlide(currentIndex + direction);
-    console.log('next' + currentIndex);
-    imgsRef.current.style =
-      'transform: translateX(' + 1215 * currentIndex + 'px)';
-  };
+
   return (
     <>
       <SpotSliderBlock>
         <SliderImgs ref={imgsRef}>
-          <SliderImg />
+          <SliderImg>
+            <div>1</div>
+            <h1>천안시장</h1>
+          </SliderImg>
+          <SliderImg>
+            <div>2</div>
+            <h1>안산드레스</h1>
+          </SliderImg>
+          <SliderImg>
+            <div>3</div>
+            <h1>마계인천</h1>
+          </SliderImg>
         </SliderImgs>
       </SpotSliderBlock>
-      <PrevButton icon={faAngleLeft} onClick={() => handlePrev(-1)} />
-      <NextButton icon={faAngleRight} onClick={() => handleNext(1)} />
+      <PrevButton icon={faAngleLeft} onClick={() => handleSwipe(-1)} />
+      <NextButton icon={faAngleRight} onClick={() => handleSwipe(1)} />
     </>
   );
 };
