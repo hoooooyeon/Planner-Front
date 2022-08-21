dabc761055cb8d04d5b01ae587167595d572d36e
hooooyeon <lhy3476@gmail.com>
2022-08-21 22:05:45 +0900

    SpotMenu 진행중

--- src/components/spot/SpotMenu.js
+++ src/components/spot/SpotMenu.js
@@ -1,9 +1,94 @@
 import styled from 'styled-components';
+import Button from '../common/Button';
 
-const SpotMenuBlock = styled.div``;
+const SpotMenuBlock = styled.div`
+  margin: 50px auto;
+  border: 1px solid red;
+  width: 90%;
+  height: 400px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  div {
+    display: flex;
+  }
+  p {
+    width: 70px;
+  }
+  ul {
+    list-style: none;
+    line-height: 10px;
+    li {
+      float: left;
+      margin: 0 15px 10px 0;
+    }
+  }
+`;
+
+const StyledButton = styled(Button)`
+  width: auto;
+`;
 
 const SpotMenu = () => {
-  return <SpotMenuBlock></SpotMenuBlock>;
+  return (
+    <SpotMenuBlock>
+      <div>
+        <p>나라</p>
+        <ul>
+          <li>
+            <StyledButton>대한민국</StyledButton>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>도시</p>
+        <ul>
+          <li>
+            <StyledButton>서울</StyledButton>
+          </li>
+          <li>
+            <StyledButton>대전</StyledButton>
+          </li>
+          <li>
+            <StyledButton>대구</StyledButton>
+          </li>
+          <li>
+            <StyledButton>부산</StyledButton>
+          </li>
+          <li>
+            <StyledButton>천안</StyledButton>
+          </li>
+          <li>
+            <StyledButton>성남</StyledButton>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>관광지</p>
+        <ul>
+          <li>
+            <StyledButton>천안 시내</StyledButton>
+          </li>
+          <li>
+            <StyledButton>천안 은행나무의 맹세</StyledButton>
+          </li>
+          <li>
+            <StyledButton>천안 하나로마트</StyledButton>
+          </li>
+          <li>
+            <StyledButton>천안 터미널 깊은 골목 길</StyledButton>
+          </li>
+          <li>
+            <StyledButton>천안 고 김따수 가</StyledButton>
+          </li>
+          <li>
+            <StyledButton>천안 언저리</StyledButton>
+          </li>
+        </ul>
+      </div>
+    </SpotMenuBlock>
+  );
 };
 
 export default SpotMenu;
--- src/components/spot/SpotSlider.js
+++ src/components/spot/SpotSlider.js
@@ -5,15 +5,15 @@ import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useRef, useState } from 'react';
 
 const SpotSliderBlock = styled.div`
-  width: 1215px;
+  width: 100%;
   height: 600px;
   border: 1px solid green;
   overflow: hidden;
   h1 {
     color: white;
     position: relative;
-    left: 45%;
     top: 80%;
+    left: 45%;
   }
 `;
 
@@ -46,37 +46,18 @@ const NextButton = styled(StyledFontAwesomeIcon)`
   right: 11%;
 `;
 
-// 슬라이더 버튼
 const TOTAL_SLIDES = 5;
 const SpotSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(2);
   const imgsRef = useRef();
 
-  const handleSlide = (currentIndex) => {
-    // if (currentIndex > TOTAL_SLIDES) {
-    //   currentIndex = TOTAL_SLIDES;
-    // } else if (currentIndex === 0) {
-    //   currentIndex = TOTAL_SLIDES-1;
-    // }
-    setCurrentIndex(currentIndex);
-    // imgsRef.current.style = 'transform: translateX(-' + 1215 * (currentIndex - 1) + 'px)';
-    // imgsRef.current.style.transition = 'all 0.5s ease-in-out';
-  };
-
+  // 슬라이더 버튼
   const handleSwipe = (direction) => {
-    handleSlide(currentIndex + direction);
+    setCurrentIndex(currentIndex + direction);
   };
 
-  // 자동 슬라이더
+  // 무한 슬라이더
   useEffect(() => {
-    const timeoutId = setInterval(() => {
-      if (currentIndex < TOTAL_SLIDES) {
-        setCurrentIndex((currentIndex) => currentIndex + 1);
-      } else {
-        setCurrentIndex(1);
-      }
-    }, 2500);
-
     if (currentIndex === 1) {
       setTimeout(() => {
         imgsRef.current.style = 'transform: translateX(-' + 1215 * (TOTAL_SLIDES - 2) + 'px)';
@@ -91,28 +72,20 @@ const SpotSlider = () => {
         setCurrentIndex(2);
       }, 800);
     }
+
+    // 자동 슬라이더
+    const timeoutId = setInterval(() => {
+      if (currentIndex < TOTAL_SLIDES) {
+        setCurrentIndex((currentIndex) => currentIndex + 1);
+      } else {
+        setCurrentIndex(1);
+      }
+    }, 2500);
+
     return () => clearInterval(timeoutId);
   }, [currentIndex]);
 
-  // 이미지 슬라이더 스타일 변경
-  // useEffect(() => {
-  //   if (currentIndex === 1) {
-  //     setTimeout(() => {
-  //       imgsRef.current.style = 'transform: translateX(-' + 1215 * (TOTAL_SLIDES - 2) + 'px)';
-  //       imgsRef.current.style.transition = '0s';
-  //       setCurrentIndex(TOTAL_SLIDES - 1);
-  //     }, 1000);
-  //   }
-
-  //   if (currentIndex === TOTAL_SLIDES) {
-  //     setTimeout(() => {
-  //       imgsRef.current.style = 'transform: translateX(-' + 1215 + 'px)';
-  //       imgsRef.current.style.transition = '0s';
-  //       setCurrentIndex(2);
-  //     }, 1000);
-  //   }
-  // }, [currentIndex]);
-
+  // 슬라이더 스타일 변경
   useEffect(() => {
     imgsRef.current.style = 'transform: translateX(-' + 1215 * (currentIndex - 1) + 'px)';
     imgsRef.current.style.transition = 'all 0.5s ease-in-out';
