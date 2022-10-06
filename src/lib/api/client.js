24168b01bd684cd7f038ef07338f88fbd00aa2f2
bluebear <showmeii1201@naver.com>
2022-10-06 14:51:36 +0900

    axios 토큰 헤더 수정

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const client = axios.create();
 
 export const tokenUse = () => {
-    client.defaults.headers.common['Authorization'] = localStorage.getItem("token");
+    client.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("token")}`;
 }
 
 export default client;
