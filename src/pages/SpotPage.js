8876624b33fdaf205943b56fbb7fb5cb3afc519e
hooooyeon <lhy3476@gmail.com>
2022-10-15 22:08:12 +0900

    여행지 슬라이더 리덕스

--- src/pages/SpotPage.js
+++ src/pages/SpotPage.js
@@ -1,16 +1,16 @@
 import Spot from '../components/spot/Spot';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
-import SpotSlider from '../components/spot/SpotSlider';
 import SpotListContainer from '../containers/spot/SpotListContainer';
 import SpotPaginationContainer from '../containers/spot/SpotPaginationContainer';
+import SpotSliderContainer from '../containers/spot/SpotSliderContainer';
 
 const SpotPage = () => {
     return (
         <>
             <Header />
             <Spot>
-                <SpotSlider />
+                <SpotSliderContainer />
                 <SpotListContainer />
                 <SpotPaginationContainer />
             </Spot>
