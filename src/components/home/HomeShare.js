094a9df4afd1842ab6e053558a62d5b172e7a6fd
hooooyeon <lhy3476@gmail.com>
2022-11-16 19:27:36 +0900

    test

--- src/components/home/HomeShare.js
+++ src/components/home/HomeShare.js
@@ -1,36 +1,114 @@
 import styled from 'styled-components';
 
 const HomeShareBlock = styled.div`
-  border: 1px solid red;
-  width: calc(100% - 40px);
+  width: 100%;
   height: 100%;
-  margin-top: 50px;
-  padding: 0 20px;
+  margin: 50px auto;
 `;
 
-const ShareList = styled.div`
-  border: 1px solid blue;
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  @media all and (min-width: 768px) {
+    width: calc(100% - 40px);
+    padding: 0 20px;
+  }
+  @media all and (min-width: 960px) {
+    width: 930px;
+    padding: 0;
+  }
+  @media all and (min-width: 1280px) {
+    width: 1250px;
+    padding: 0;
+  }
+`;
+
+const ShareList = styled.ul`
+  list-style-type: none;
   width: 100%;
   height: 100%;
   display: flex;
+  padding: 0;
+  margin: 0 auto;
 `;
 
-const ShareItem = styled.div`
-  border: 1px solid green;
+const ShareItem = styled.li`
   width: 24%;
+  flex-shrink: 0;
+  margin: 0.5%;
+  box-shadow: 3px 3px 7px 1px rgb(0 0 0 / 30%);
+  border-radius: 0.5rem;
+`;
+
+const InfoBox = styled.div`
+  user-select: none;
+  height: 60px;
+  margin: 0;
+  padding: 3px;
+  border-top: 1px solid lightgray;
+`;
+
+const Map = styled.div`
+  width: 100%;
   height: 150px;
+  border: none;
+  margin: auto;
+  @media all and (min-width: 960px) {
+    height: 200px;
+  }
+  @media all and (min-width: 1280px) {
+    height: 250px;
+  }
+`;
+
+const Name = styled.p`
+  margin: 0 0 10px 0;
+  font-weight: bold;
+`;
+
+const Date = styled.p`
+  margin: 0;
+  font-size: 0.8rem;
+  color: gray;
 `;
 
 const HomeShare = () => {
   return (
     <HomeShareBlock>
-      <h3>사용자들의 플래너</h3>
-      <ShareList>
-        <ShareItem />
-        <ShareItem />
-        <ShareItem />
-        <ShareItem />
-      </ShareList>
+      <Container>
+        <h3>사용자들의 플래너</h3>
+        <ShareList>
+          <ShareItem>
+            <Map />
+            <InfoBox>
+              <Name>1</Name>
+              <Date>2020년 11월 11일 ~ 2022년 17월 29일</Date>
+            </InfoBox>
+          </ShareItem>
+          <ShareItem>
+            <Map />
+            <InfoBox>
+              <Name>1</Name>
+              <Date>2020년 11월 11일 ~ 2022년 17월 29일</Date>
+            </InfoBox>
+          </ShareItem>
+          <ShareItem>
+            <Map />
+            <InfoBox>
+              <Name>1</Name>
+              <Date>2020년 11월 11일 ~ 2022년 17월 29일</Date>
+            </InfoBox>
+          </ShareItem>
+          <ShareItem>
+            <Map />
+            <InfoBox>
+              <Name>1</Name>
+              <Date>2020년 11월 11일 ~ 2022년 17월 29일</Date>
+            </InfoBox>
+          </ShareItem>
+        </ShareList>
+      </Container>
     </HomeShareBlock>
   );
 };
--- src/components/home/HomeSpot.js
+++ src/components/home/HomeSpot.js
@@ -1,13 +1,83 @@
 import styled from 'styled-components';
 
 const HomeSpotBlock = styled.div`
-  
+  width: calc(100% - 40px);
+  height: 100%;
+  margin: 50px auto;
+  padding: 0 20px;
+
+`
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  @media all and (min-width: 768px) {
+    width: calc(100% - 40px);
+    padding: 0 20px;
+  }
+  @media all and (min-width: 960px) {
+    width: 930px;
+    padding: 0;
+  }
+  @media all and (min-width: 1280px) {
+    width: 1250px;
+    padding: 0;
+  }
+`
+const HiddenBox = styled.div`
+  margin: 0 auto;
+  overflow: hidden;
+  z-index: 1;
+  @media all and (max-width: 768px) {
+    margin-left: 15px;
+  }
+  @media all and (min-width: 768px) {
+    width: calc(100% - 40px);
+  }
+  @media all and (min-width: 1025px) {
+    width: 100%;
+  }
+`
+
+const SpotList = styled.ul`
+list-style-type: none;
+width: 840px;
+  height: 100%;
+  margin: 0 auto;
+  padding: 0;
+  display: inline-block;
+  @media all and (min-width: 768px) {
+    width: 100%;
+  }
+`
+
+const SpotItem = styled.li`
+  flex-shrink: 0;
+  width: 200px;
+  float: left; 
+  box-shadow: 3px 3px 7px 1px rgb(0,0,0,30%);
+  border-radius: 0.5rem;
+  margin: 0.5%;
+  @media all and (min-width: 768px) {
+    width: 24%;
+  }
+  &:hover {
+    cursor: pointer;
+  }
 `
 
 const HomeSpot = () => {
   return (
     <HomeSpotBlock>
-
+      <Container>
+        <h3>여행지</h3>
+        <HiddenBox>
+        <SpotList>
+          <SpotItem />
+        </SpotList>
+        </HiddenBox>
+      </Container>
     </HomeSpotBlock>
   )
 }
