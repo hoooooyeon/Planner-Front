dc4bc59079eee4f46e43704e2d7f31ce7705e022
hooooyeon <lhy3476@gmail.com>
2022-12-27 00:23:54 +0900

    메인페이지 intro 수정중

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Button from '../common/Button';
+// import video from '../../videos/intro_city_night.mp4'
 
 const IntroBlock = styled.div`
   width: 100%;
@@ -15,20 +15,35 @@ const IntroBlock = styled.div`
   }
 `;
 
-const StyledButton = styled(Button)`
+const Button = styled.button`
   width: 150px;
+  height: 50px;
   margin: 50px 0;
+  border: none;
+  border-radius: 10px;
+  background-color: #9AAD67;
+  color: white;
+font-size: 0.9rem;
+font-weight: bold;
 `;
+
+// const Video = styled.video`
+//   width: 200px;
+//   height: 200px;
+// `;
 
 const Intro = () => {
   return (
     <IntroBlock>
+      <video muted autoPlay loop>
+        <source src="videos/intro_city_night.mp4" type="videos.mp4" />
+      </video>
       <div>
         <p>가족, 친구 혹은 연인과 함께 여행을 계획해 보세요.</p>
         <p>다른 사람들과 여행 일정을 공유 해보세요.</p>
         <p>다른 사람들이 여행했던 루트를 참고하여 쉽고 빠르게 계획해 보세요.</p>
       </div>
-      <StyledButton big>플래너 만들기</StyledButton>
+      <Button big>플래너 만들기</Button>
     </IntroBlock>
   );
 };
