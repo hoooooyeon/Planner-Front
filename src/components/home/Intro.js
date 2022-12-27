d4efd79ab62f008183ecc1aac29b3211e2f9b071
hooooyeon <lhy3476@gmail.com>
2022-12-27 18:06:38 +0900

    메인페이지 intro 수정 완

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -1,18 +1,35 @@
 import styled from 'styled-components';
-// import video from '../../videos/intro_city_night.mp4'
+import ReactPlayer from 'react-player';
 
 const IntroBlock = styled.div`
   width: 100%;
-  height: 650px;
+  height: 680px;
   display: flex;
+  margin-top: 75px;
+  position: relative;
+  
+  overflow: hidden;
+  `;
+
+const TextBox = styled.div`
+  width: 250px;
+  height: 100%;
+  background-color: #CDD9AC;
+  display:  flex;
   flex-direction: column;
   align-items: center;
-  justify-content: flex-end;
-  margin-top: 75px;
-  div {
-    display: flex;
-    flex-direction: column;
-  }
+  justify-content: center;
+  padding:  0 50px;
+  position: absolute;
+  right: 0;
+  top: 0;
+`
+
+const IntroText = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: white;
+  font-weight: bold;
 `;
 
 const Button = styled.button`
@@ -21,10 +38,16 @@ const Button = styled.button`
   margin: 50px 0;
   border: none;
   border-radius: 10px;
-  background-color: #9AAD67;
+  background-color: #9aad67;
   color: white;
-font-size: 0.9rem;
-font-weight: bold;
+  font-size: 0.9rem;
+  font-weight: bold;
+`;
+
+const IntroVideo = styled(ReactPlayer)`
+  position: absolute;
+  top: 0;
+  left: 0;
 `;
 
 // const Video = styled.video`
@@ -35,15 +58,20 @@ font-weight: bold;
 const Intro = () => {
   return (
     <IntroBlock>
-      <video muted autoPlay loop>
-        <source src="videos/intro_city_night.mp4" type="videos.mp4" />
-      </video>
-      <div>
+      {/* 
+       video와 source 태그 사용이 안됨. 이유 아직 모름.
+      <video  autoPlay muted loop>
+        <source src="/videos/intro_city_night.mp4" type="videos/mp4" />
+      </video> */}
+      <IntroVideo playing muted loop url={'/videos/intro_city.mp4'} width="1200px" height="100%" />
+      <TextBox>
+      <IntroText>
         <p>가족, 친구 혹은 연인과 함께 여행을 계획해 보세요.</p>
-        <p>다른 사람들과 여행 일정을 공유 해보세요.</p>
-        <p>다른 사람들이 여행했던 루트를 참고하여 쉽고 빠르게 계획해 보세요.</p>
-      </div>
+        <p>다른 이용자들에게 여행 일정을 공유 해보세요.</p>
+        <p>다른 이용자들이 여행했던 루트를 참고하여 쉽고 빠르게 계획해 보세요.</p>
+      </IntroText>
       <Button big>플래너 만들기</Button>
+      </TextBox>
     </IntroBlock>
   );
 };
