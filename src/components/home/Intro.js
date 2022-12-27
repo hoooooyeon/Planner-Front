87b0cf2a3f5271d03c209dd1bb4d9b3f9a9e8a63
hooooyeon <lhy3476@gmail.com>
2022-12-27 19:25:42 +0900

    메인페이지 인트로 수정 완2

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import ReactPlayer from 'react-player';
+import { useEffect, useState } from 'react';
 
 const IntroBlock = styled.div`
   width: 100%;
@@ -7,29 +8,34 @@ const IntroBlock = styled.div`
   display: flex;
   margin-top: 75px;
   position: relative;
-  
-  overflow: hidden;
-  `;
 
-const TextBox = styled.div`
+  overflow: hidden;
+`;
+
+const IntroBox = styled.div`
   width: 250px;
   height: 100%;
-  background-color: #CDD9AC;
-  display:  flex;
+  background-color: #cdd9ac;
+  display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding:  0 50px;
+  padding: 0 50px;
   position: absolute;
   right: 0;
   top: 0;
-`
+  font-weight: bold;
+  color: white;
+`;
 
-const IntroText = styled.div`
+const Title = styled.div`
+  font-size: 3rem;
+  margin-bottom: 40px;
+`;
+
+const Text = styled.div`
   display: flex;
   flex-direction: column;
-  color: white;
-  font-weight: bold;
 `;
 
 const Button = styled.button`
@@ -39,9 +45,9 @@ const Button = styled.button`
   border: none;
   border-radius: 10px;
   background-color: #9aad67;
+  font-weight: bold;
   color: white;
   font-size: 0.9rem;
-  font-weight: bold;
 `;
 
 const IntroVideo = styled(ReactPlayer)`
@@ -56,6 +62,14 @@ const IntroVideo = styled(ReactPlayer)`
 // `;
 
 const Intro = () => {
+  const [isVideo, setIsVideo] = useState();
+  useEffect(() => {
+    const videoArr = ['/videos/intro_city.mp4', '/videos/intro_train.mp4', '/videos/intro_waterfall.mp4'];
+    let num = Math.round(Math.random() * (videoArr.length-1));
+
+    setIsVideo(videoArr[num]);
+  }, []);
+
   return (
     <IntroBlock>
       {/* 
@@ -63,15 +77,16 @@ const Intro = () => {
       <video  autoPlay muted loop>
         <source src="/videos/intro_city_night.mp4" type="videos/mp4" />
       </video> */}
-      <IntroVideo playing muted loop url={'/videos/intro_city.mp4'} width="1200px" height="100%" />
-      <TextBox>
-      <IntroText>
-        <p>가족, 친구 혹은 연인과 함께 여행을 계획해 보세요.</p>
-        <p>다른 이용자들에게 여행 일정을 공유 해보세요.</p>
-        <p>다른 이용자들이 여행했던 루트를 참고하여 쉽고 빠르게 계획해 보세요.</p>
-      </IntroText>
-      <Button big>플래너 만들기</Button>
-      </TextBox>
+      <IntroVideo playing muted loop url={isVideo} width="1200px" height="100%" />
+      <IntroBox>
+        <Title>한국다봄</Title>
+        <Text>
+          <p>가족, 친구 혹은 연인과 함께 여행을 계획해 보세요.</p>
+          <p>다른 이용자들에게 여행 일정을 공유 해보세요.</p>
+          <p>다른 이용자들이 여행했던 루트를 참고하여 쉽고 빠르게 계획해 보세요.</p>
+        </Text>
+        <Button big>플래너 만들기</Button>
+      </IntroBox>
     </IntroBlock>
   );
 };
