19f37bc0571d800fb3f5650f7654050ad8ee70ba
hooooyeon <lhy3476@gmail.com>
2023-01-03 22:08:30 +0900

    슬라이더 스크롤 수정 완

--- src/components/spot/SpotList.js
+++ src/components/spot/SpotList.js
@@ -87,6 +87,29 @@ const List = styled.ul`
     }
 `;
 
+const ScrollBox = styled.div`
+    width: calc(100% - 40px);
+    height: 4px;
+    margin-top: 5px;
+    border-radius: 10px;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: lightgray;
+    overflow: hidden;
+    z-index: 1;
+    opacity: 0;
+    @media all and (min-width: 768px) {
+        display: none;
+    }
+`;
+
+const Scroll = styled.div`
+    width: 50%;
+    height: 100%;
+    background-color: gray;
+`;
+
 const SpotList = ({ areas, spots, spotError, detail, currentInfo, onFirstSpotsPage, onUnloadDetailSpot, onToggleLikeSpot, onOpenDetail }) => {
     // 대체 이미지 넣기
     const onChangeErrorImg = (e) => {
@@ -95,8 +118,6 @@ const SpotList = ({ areas, spots, spotError, detail, currentInfo, onFirstSpotsPa
 
     const menuRef = useRef();
     const menuBoxRef = useRef();
-    const listRef = useRef();
-    const listBoxRef = useRef();
 
     // ---------- 메뉴 슬라이더 ----------
     let mIsSlide = false;
@@ -116,7 +137,7 @@ const SpotList = ({ areas, spots, spotError, detail, currentInfo, onFirstSpotsPa
             mMoveX = mSliderX + mCurrentX - mStartX;
 
             menuRef.current.style.transform = ' translateX(' + mMoveX + 'px)';
-            menuRef.current.style.transitionDuration = '0s';
+            menuRef.current.style.transitionDuration = '0ms';
         }
     };
 
@@ -129,20 +150,19 @@ const SpotList = ({ areas, spots, spotError, detail, currentInfo, onFirstSpotsPa
         }
 
         menuRef.current.style.transform = 'translateX(' + mSliderX + 'px)';
-        menuRef.current.style.transitionDuration = ' 1s';
+        menuRef.current.style.transitionDuration = ' 1000ms';
 
         mIsSlide = false;
     };
 
     const menuSliderResize = () => {
-
         if (mSliderX > 0) {
             mSliderX = 0;
         } else if (mSliderX < menuBoxRef.current.clientWidth - menuRef.current.scrollWidth) {
             mSliderX = menuBoxRef.current.clientWidth - menuRef.current.scrollWidth;
         }
         menuRef.current.style.transform = 'translateX(' + mSliderX + 'px)';
-        menuRef.current.style.transitionDuration = ' 1s';
+        menuRef.current.style.transitionDuration = ' 1000ms';
     };
 
     useEffect(() => {
@@ -163,12 +183,20 @@ const SpotList = ({ areas, spots, spotError, detail, currentInfo, onFirstSpotsPa
     });
 
     // ---------- 여행지 슬라이더 ----------
+
+    const listRef = useRef();
+    const listBoxRef = useRef();
+    const scrollBoxRef = useRef();
+    const scrollRef = useRef();
+
     let lIsSlide = false;
     let lStartX = 0;
     let lCurrentX = 0;
     let lMoveX = 0;
     let lSliderX = 0;
     const TOTAL_SLIDE = 4;
+
+    let scrollMoveX = 0;
 
     const listSliderStart = (e) => {
         lStartX = e.clientX;
@@ -181,7 +209,20 @@ const SpotList = ({ areas, spots, spotError, detail, currentInfo, onFirstSpotsPa
             lMoveX = lSliderX + lCurrentX - lStartX;
 
             listRef.current.style.transform = 'translateX(' + lMoveX + 'px)';
-            listRef.current.style.transitionDuration = '0s';
+            listRef.current.style.transitionDuration = '0ms';
+
+            scrollMoveX = -((lMoveX / -(listBoxRef.current.clientWidth - listRef.current.clientWidth)) * 100);
+
+            if (scrollMoveX < 0) {
+                scrollMoveX = 0;
+            } else if (scrollMoveX > 100) {
+                scrollMoveX = 100;
+            }
+
+            scrollBoxRef.current.style.opacity = 1;
+            scrollBoxRef.current.style.transitionDuration = '400ms';
+            scrollRef.current.style.transform = 'translateX(' + scrollMoveX + '%)';
+            scrollRef.current.style.transitionDuration = '0ms';
         }
     };
 
@@ -196,7 +237,10 @@ const SpotList = ({ areas, spots, spotError, detail, currentInfo, onFirstSpotsPa
         }
 
         listRef.current.style.transform = 'translateX(' + lSliderX + 'px)';
-        listRef.current.style.transitionDuration = ' 1s';
+        listRef.current.style.transitionDuration = ' 1000ms';
+
+        scrollBoxRef.current.style.opacity = 0;
+        scrollBoxRef.current.style.transitionDuration = '2000ms';
         lIsSlide = false;
     };
 
@@ -208,7 +252,7 @@ const SpotList = ({ areas, spots, spotError, detail, currentInfo, onFirstSpotsPa
         }
 
         listRef.current.style.transform = 'translateX(' + lSliderX + 'px)';
-        listRef.current.style.transitionDuration = ' 1s';
+        listRef.current.style.transitionDuration = '1000ms';
     };
 
     useEffect(() => {
@@ -240,10 +284,8 @@ const SpotList = ({ areas, spots, spotError, detail, currentInfo, onFirstSpotsPa
                     {areas && (
                         <Menu ref={menuRef}>
                             {areas.map((area) => (
-
                                 <li key={area.code} onClick={(e) => onFirstSpotsPage(e, area.code)} aria-current={areaNum === area.code ? 'page' : null}>
                                     {area.name}
-                                    
                                 </li>
                             ))}
                         </Menu>
@@ -258,6 +300,9 @@ const SpotList = ({ areas, spots, spotError, detail, currentInfo, onFirstSpotsPa
                                 ))}
                             </List>
                         </HiddenBox>
+                        <ScrollBox ref={scrollBoxRef}>
+                            <Scroll ref={scrollRef} />
+                        </ScrollBox>
                     </>
                 )}
                 {detail && <SpotDetailModal detail={detail} onChangeErrorImg={onChangeErrorImg} onUnloadDetailSpot={onUnloadDetailSpot} onToggleLikeSpot={onToggleLikeSpot} />}
