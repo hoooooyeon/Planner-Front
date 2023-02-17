c53bed93d4e9068fb04452c8e9df5b208dbeb2ae
hooooyeon <lhy3476@gmail.com>
2023-02-17 20:36:09 +0900

    Revert "test"
    This reverts commit 7ac5cafc2783f38f547c37b2ea8b5ceb1c6a352f.

--- src/lib/createRequestSaga.js
+++ src/lib/createRequestSaga.js
@@ -1,22 +1,22 @@
 import { call, put } from 'redux-saga/effects';
 
 export default function createRequestSaga(type, request) {
-    const SUCCESS = `${type}_SUCCESS`;
-    const FAILURE = `${type}_FAILURE`;
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
 
-    return function* (action) {
-        try {
-            const response = yield call(request, action);
-            yield put({
-                type: SUCCESS,
-                payload: response.data,
-            });
-        } catch (e) {
-            yield put({
-                type: FAILURE,
-                payload: e,
-                error: true,
-            });
-        }
-    };
+  return function* (action) {
+    try {
+      const response = yield call(request, action);
+      yield put({
+        type: SUCCESS,
+        payload: response.data,
+      });
+    } catch (e) {
+      yield put({
+        type: FAILURE,
+        payload: e,
+        error: true,
+      });
+    }
+  };
 }
