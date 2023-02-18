df6b3b8749bd562b2377d5a0aef25dff3cce7e26
hooooyeon <lhy3476@gmail.com>
2023-02-19 00:33:08 +0900

    플래너브랜치 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import './App.css';
 import { Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import SpotPage from './pages/SpotPage';
 import LoginPage from './pages/account/LoginPage';
 import RegisterPage from './pages/account/RegisterPage';
 import ProfilePage from './pages/account/ProfilePage';
-import MyLikePage from './pages/account/MyLikePage';
 
 const App = () => {
-  return (
-    <>
-      <Route path="/" component={HomePage} exact />
-      <Route path="/Spot" component={SpotPage} />
-      <Route path="/Login" component={LoginPage} />
-      <Route path="/Register" component={RegisterPage} />
-      <Route path="/Profile" component={ProfilePage} />
-      <Route path="/MyLike" component={MyLikePage} />
-    </>
-  );
+    return (
+        <>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/Login" component={LoginPage} />
+            <Route path="/Register" component={RegisterPage} />
+            <Route path="/Profile" component={ProfilePage} />
+        </>
+    );
 };
 
 export default App;
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,9 @@
-import { combineReducers } from "redux";
-import authReducer from "./authModule";
-import profileReducer, { profileSaga } from "./profileModule";
+import { combineReducers } from 'redux';
+import authReducer from './authModule';
+import profileReducer, { profileSaga } from './ProfileModule';
 import { all } from 'redux-saga/effects';
-import { authSaga } from "./authModule";
-import loadingReducer from "./loadingModule";
+import { authSaga } from './authModule';
+import loadingReducer from './loadingModule';
 
 const rootReducer = combineReducers({
     loadingReducer,
@@ -13,6 +13,6 @@ const rootReducer = combineReducers({
 
 export function* rootSaga() {
     yield all([authSaga(), profileSaga()]);
-};
+}
 
 export default rootReducer;
