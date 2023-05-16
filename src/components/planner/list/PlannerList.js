a4c3e8bb34ea64732abbd27b454d73b2cc94b954
hooooyeon <lhy3476@gmail.com>
2023-05-16 15:40:36 +0900

    공용 페이지네이션 생성#16

--- src/components/planner/list/PlannerList.js
+++ src/components/planner/list/PlannerList.js
@@ -1,13 +1,11 @@
 import MyPlannerList from './MyPlannerList';
 import ShareList from './ShareList';
-import ListPagination from './ListPagination';
 
 const PlannerList = ({ myPlanners, sharePlanners, plannerError, onCreatePlanner, onLoadPlanner, onResetPlannerInfoForm, onChangeCurPlannerId }) => {
     return (
         <>
             <MyPlannerList myPlanners={myPlanners} onCreatePlanner={onCreatePlanner} onResetPlannerInfoForm={onResetPlannerInfoForm} onChangeCurPlannerId={onChangeCurPlannerId} />
             <ShareList sharePlanners={sharePlanners} onLoadPlanner={onLoadPlanner} plannerError={plannerError} onChangeCurPlannerId={onChangeCurPlannerId} />
-            {/* <ListPagination /> */}
         </>
     );
 };
