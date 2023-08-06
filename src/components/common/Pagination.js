e9ab4fd3024214a7299f4fdcba5ea1a70a2b22da
hoooooyeon <lhy3476@gmail.com>
2023-08-06 23:54:23 +0900

    페이지네이션 수정중

--- src/components/common/Pagination.js
+++ src/components/common/Pagination.js
@@ -51,7 +51,32 @@ const PageButton = styled.div`
     }
 `;
 
-const Pagination = ({ pageArr, onIndexPage, onNextPage, onPreviousPage, onFirstPage, onLastPage }) => {
+const Pagination = ({ page, totalCount, onIndexPage, onNextPage, onPreviousPage, onFirstPage, onLastPage }) => {
+    // 뿌려줄 페이지 배열
+    const [pageArr, setPageArr] = useState([]);
+    // 페이지의 10단위
+    const [block, setBlock] = useState(0);
+    // 보여질 페이지네이션의 개수
+    const limitIndex = 5;
+    // 마지막 페이지
+    const maxPage = Math.ceil(totalCount / limitIndex);
+
+    // 페이지네이션 배열 생성
+    useEffect(() => {
+        const arr = Array.from({ length: maxPage }, (_, i) => i + 1);
+
+        setPageArr(arr.slice(limitIndex * block, limitIndex * (block + 1)));
+    }, [block, maxPage]);
+
+    // block 처리
+    useEffect(() => {
+        if (page === 1) {
+            setBlock(0);
+        } else if (page === maxPage) {
+            setBlock(Math.ceil(maxPage / limitIndex - 1));
+        }
+    }, [page]);
+
     return (
         <PaginationBlock>
             <Container>
@@ -65,15 +90,23 @@ const Pagination = ({ pageArr, onIndexPage, onNextPage, onPreviousPage, onFirstP
                         </PageButton>
                         {pageArr.map((i) => {
                             return (
-                                <PageButton key={i} onClick={() => onIndexPage(i)}>
+                                <PageButton
+                                    key={i}
+                                    aria-current={page === i ? 'cur' : null}
+                                    onClick={() => onIndexPage(i)}
+                                >
                                     {i}
                                 </PageButton>
                             );
                         })}
-                        <PageButton onClick={onNextPage}>
+                        <PageButton
+                            onClick={() => {
+                                onNextPage(maxPage);
+                            }}
+                        >
                             <FontAwesomeIcon icon={faCaretRight} />
                         </PageButton>
-                        <PageButton onClick={onLastPage}>
+                        <PageButton onClick={() => onLastPage(maxPage)}>
                             <FontAwesomeIcon icon={faForward} />
                         </PageButton>
                     </>
--- src/containers/spot/SpotPaginationContainer.js
+++ src/containers/spot/SpotPaginationContainer.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import Pagination from '../../components/common/Pagination.js';
 import { changePageIndexAction } from '../../modules/spotModule';
@@ -7,57 +7,34 @@ const SpotPaginationContainer = ({ spots, spotData, changePageIndexAction }) => 
     const { totalCount } = { ...spots };
     const { pageNo } = { ...spotData };
 
-    // 뿌려줄 페이지 배열
-    const [pageArr, setPageArr] = useState([]);
-    // 페이지의 10단위
-    const [block, setBlock] = useState(0);
-    // 보여질 페이지네이션의 개수
-    const limitIndex = 5;
-    // 마지막 페이지
-    const maxPage = Math.ceil(totalCount / limitIndex);
     // 현재 페이지
     const [page, setPage] = useState(1);
-
-    // 페이지네이션 배열 생성 함수
-    useEffect(() => {
-        const arr = Array.from({ length: maxPage }, (_, i) => i + 1);
-
-        setPageArr(arr.slice(limitIndex * block, limitIndex * (block + 1)));
-    }, [block, maxPage]);
-
-    useEffect(() => {
-        if (pageNo === 1) {
-            setBlock(0);
-        } else if (pageNo === maxPage) {
-            setBlock(Math.ceil(maxPage / limitIndex - 1));
-        }
-    }, [pageNo, maxPage]);
 
     const onIndexPage = (index) => {
         setPage(index);
     };
-    const onNextPage = () => {
-        if (!(page === maxPage)) {
+    const onNextPage = (maxPage) => {
+        if (page < maxPage) {
             setPage((index) => index + 1);
-            if (pageNo % limitIndex === 0) {
-                setBlock((block) => block + 1);
-            }
         }
     };
     const onPreviousPage = () => {
-        if (!(page === 1)) {
+        if (page > 1) {
             setPage((index) => index - 1);
-            if (page % limitIndex === 1) {
-                setBlock((block) => block - 1);
-            }
         }
     };
     const onFirstPage = () => {
         setPage(1);
     };
-    const onLastPage = () => {
+    const onLastPage = (maxPage) => {
         setPage(maxPage);
     };
+
+    // else if (page % limitIndex === 0) {
+    //     setBlock((block) => block + 1);
+    // } else if (page % limitIndex === 1) {
+    //     setBlock((block) => block - 1);
+    // }
 
     useEffect(() => {
         changePageIndexAction(page);
@@ -65,12 +42,13 @@ const SpotPaginationContainer = ({ spots, spotData, changePageIndexAction }) => 
 
     return (
         <Pagination
-            pageArr={pageArr}
             onIndexPage={onIndexPage}
             onNextPage={onNextPage}
             onPreviousPage={onPreviousPage}
             onFirstPage={onFirstPage}
             onLastPage={onLastPage}
+            page={page}
+            totalCount={totalCount}
         />
     );
 };
@@ -84,3 +62,90 @@ export default connect(
         changePageIndexAction,
     },
 )(SpotPaginationContainer);
+
+// import { useEffect, useState } from 'react';
+// import { connect } from 'react-redux';
+// import Pagination from '../../components/common/Pagination.js';
+// import { changePageIndexAction } from '../../modules/spotModule';
+
+// const SpotPaginationContainer = ({ spots, spotData, changePageIndexAction }) => {
+//     const { totalCount } = { ...spots };
+//     const { pageNo } = { ...spotData };
+
+//     // 뿌려줄 페이지 배열
+//     const [pageArr, setPageArr] = useState([]);
+//     // 페이지의 10단위
+//     const [block, setBlock] = useState(0);
+//     // 보여질 페이지네이션의 개수
+//     const limitIndex = 5;
+//     // 마지막 페이지
+//     const maxPage = Math.ceil(totalCount / limitIndex);
+//     // 현재 페이지
+//     const [page, setPage] = useState(1);
+
+//     // 페이지네이션 배열 생성 함수
+//     useEffect(() => {
+//         const arr = Array.from({ length: maxPage }, (_, i) => i + 1);
+
+//         setPageArr(arr.slice(limitIndex * block, limitIndex * (block + 1)));
+//     }, [block, maxPage]);
+
+//     useEffect(() => {
+//         if (pageNo === 1) {
+//             setBlock(0);
+//         } else if (pageNo === maxPage) {
+//             setBlock(Math.ceil(maxPage / limitIndex - 1));
+//         }
+//     }, [pageNo, maxPage]);
+
+//     const onIndexPage = (index) => {
+//         setPage(index);
+//     };
+//     const onNextPage = () => {
+//         if (!(page === maxPage)) {
+//             setPage((index) => index + 1);
+//             if (pageNo % limitIndex === 0) {
+//                 setBlock((block) => block + 1);
+//             }
+//         }
+//     };
+//     const onPreviousPage = () => {
+//         if (!(page === 1)) {
+//             setPage((index) => index - 1);
+//             if (page % limitIndex === 1) {
+//                 setBlock((block) => block - 1);
+//             }
+//         }
+//     };
+//     const onFirstPage = () => {
+//         setPage(1);
+//     };
+//     const onLastPage = () => {
+//         setPage(maxPage);
+//     };
+
+//     useEffect(() => {
+//         changePageIndexAction(page);
+//     }, [page, changePageIndexAction]);
+
+//     return (
+//         <Pagination
+//             pageArr={pageArr}
+//             onIndexPage={onIndexPage}
+//             onNextPage={onNextPage}
+//             onPreviousPage={onPreviousPage}
+//             onFirstPage={onFirstPage}
+//             onLastPage={onLastPage}
+//         />
+//     );
+// };
+
+// export default connect(
+//     (state) => ({
+//         spots: state.spotReducer.spots,
+//         spotData: state.spotReducer.spotData,
+//     }),
+//     {
+//         changePageIndexAction,
+//     },
+// )(SpotPaginationContainer);
