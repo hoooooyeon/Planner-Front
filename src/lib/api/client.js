95ee715893c30d0b91a848b61f11d8af0f10a5fb
taesooooo <showmeii1201@naver.com>
2023-09-06 17:04:12 +0900

    토큰 확인이 안되어 비로그인시 리스트 못불러오는 현상 수정

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 const client = axios.create();
 
 export const tokenUse = () => {
-    client.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+    if (localStorage.getItem('token') != null) {
+        client.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+
+    }
 };
 export default client;
