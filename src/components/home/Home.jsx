21d6f5bfe69dc0588f053b5a622f1abaa1445183
taesooooo <showmeii1201@naver.com>
2023-11-03 23:53:44 +0900

    #35 홈 리뷰 리스트 불러오기 추가

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,13 +4,13 @@ import HomeReviewList from './HomeReviewList';
 import HomeSpotList from './HomeSpotList';
 import HomeServices from './HomeServices';
 
-const Home = ({ sharePlanners, loading, onClickPlanner }) => {
+const Home = ({ sharePlanners, reviewList, loading, onClickPlanner, onReviewClick }) => {
     return (
         <>
             <Main />
             <HomeServices />
             <HomePlannerList sharePlanners={sharePlanners} loading={loading} onClickPlanner={onClickPlanner} />
-            <HomeReviewList loading={loading} />
+            <HomeReviewList reviewList={reviewList} loading={loading} onReviewClick={onReviewClick} />
             <HomeSpotList />
         </>
     );
